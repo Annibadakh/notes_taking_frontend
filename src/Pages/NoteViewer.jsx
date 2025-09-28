86ad508ccb47f0e7aba2ea933ae742c99ae5e9be
Aniket Badakh <aniketbadakh1212@gmail.com>
2025-09-29 03:52:06 +0530

    new change

--- src/Pages/NoteViewer.jsx
+++ src/Pages/NoteViewer.jsx
@@ -219,6 +219,7 @@ const NoteViewer = () => {
         </div>
       </main>
 
+
       {deleteModalOpen && (
         <div className="fixed inset-0 bg-black/30 backdrop-blur-sm flex items-center justify-center p-4 z-50">
           <div className="bg-white rounded-lg max-w-md w-full p-6">
